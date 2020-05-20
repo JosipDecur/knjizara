d941d83ca5becc9c4d8ab33c413b241a0e507543
JosipDecur <64425344+JosipDecur@users.noreply.github.com>
2020-05-20 18:21:50 +0200

    Osnovni kod
    Dodani su izbori potrebni za rad knjižnice

--- main.cpp
+++ main.cpp
@@ -9,19 +9,19 @@ using namespace std;
 
 int main()
 {
-    int izbor, brojac=0;
-    int brArtikla=0;
+    int izbor;
+    int brKnjige=0;
     unsigned long long int *barkod = new unsigned long long int[2000];
     string *naziv = new string[2000];
     double *cijena = new double[2000];
     while(1)
     {
-        cout << "1. Unos novog artikla: " << endl;
+        cout << "1. Unos nove knjige: " << endl;
         cout << "2. Ispis svih podataka: " << endl;
-        cout << "3. Inventura: " << endl;
-        cout << "4. Pretraga prema nazivu artikla: " << endl;
-        cout << "5. Brisanje artikla: " << endl;
-        cout << "6. Izmjena podataka artikla: " << endl;
+        cout << "3. Kupnja knjige: " << endl;
+        cout << "4. Pretraga prema nazivu knjige: " << endl;
+        cout << "5. Brisanje knjige: " << endl;
+        cout << "6. Izmjena podataka knjige: " << endl;
         cout << "7. Izlaz iz programa: " << endl;
         cout << "Odaberite: ";
         cin >> izbor;
@@ -43,13 +43,13 @@ int main()
                     cout << msg << endl;
                 }
             }
-            barkod[brArtikla]=unos;
-            cout << "Unesite naziv artikla: " << endl;
+            barkod[brKnjige]=unos;
+            cout << "Unesite naziv knjige: " << endl;
             cin.ignore();
-            getline(cin, naziv[brArtikla]);
-            cout << "Unesite cijenu artikla: " << endl;
-            cin >> cijena[brArtikla];
-            brArtikla++;
+            getline(cin, naziv[brKnjige]);
+            cout << "Unesite cijenu knjige: " << endl;
+            cin >> cijena[brKnjige];
+            brKnjige++;
         }
     }
     return 0;
